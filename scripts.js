4d3e82155d905e86ecb1927051ba1e4d6374c2e0
FaridKX <luckyhonor70@gmail.com>
2024-10-22 08:43:48 -1200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,8 +4,16 @@ const ctloader = document.getElementById('container-for-element');
 document.getElementById('first-header-button').addEventListener('click', function() {
 loader.style.display = 'block';
 ctloader.style.display = 'flex';
+
 document.getElementById('content-of-this-web-site').style.display = 'none';
         setTimeout(function() {
+            localStorage.setItem('navigated', 'true');
             window.location.href = 'zayavka/register.html';
-        }, 2000);
+        }, 500);
     });
+    
+    if
+    (localStorage.getItem('navigated')) {
+        loader.style.display = 'none';
+        localStorage.removeItem('navigated');
+    }

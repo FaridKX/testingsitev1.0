fca471141a458823cc6fa5efc9663b3c23bc3391
FaridKX <luckyhonor70@gmail.com>
2024-10-23 08:55:01 -1200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,20 +2,34 @@ const loader = document.getElementById('loader');
 const ctloader = document.getElementById('container-for-element');
 const numberbarber = document.getElementById('second-header-button');
 
-document.getElementById('first-header-button').addEventListener('click', function() {
-loader.style.display = 'block';
-ctloader.style.display = 'flex';
-document.getElementById('content-of-this-web-site').style.display = 'none';
+function hideLoader() {
+    setTimeout(function() {
+        loader.style.display = 'none';
+        document.getElementById('content-of-this-web-site').style.display = 'block';
+        document.body.style.overflow = 'visible';
+    }, 350);
+}
+
+if (localStorage.getItem('navigated')) {
+    hideLoader();
+    localStorage.removeItem('navigated');
+} else {
+
+    document.getElementById('first-header-button').addEventListener('click', function() {
+        loader.style.display = 'block';
+        ctloader.style.display = 'flex';
+        document.getElementById('content-of-this-web-site').style.display = 'none';
         
-setTimeout(function() {
+        setTimeout(function() {
             localStorage.setItem('navigated', 'true');
             window.location.href = 'zayavka/register.html';
         }, 500);
-});
-if (localStorage.getItem('navigated')) {
-        loader.style.display = 'none';
-        localStorage.removeItem('navigated');
+    });
 }
+
+window.addEventListener('load', function() {
+    hideLoader();
+});
 
 numberbarber.addEventListener('click', ()=> {
     window.location.href = 'tel: +7(996)5610404'

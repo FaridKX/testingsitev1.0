6472335ddc4fb97beff8ca8c8028ed2ad08c7952
FaridKX <luckyhonor70@gmail.com>
2024-10-25 08:42:36 -1200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,34 +3,36 @@ const ctloader = document.getElementById('container-for-element');
 const numberbarber = document.getElementById('second-header-button');
 
 function hideLoader() {
-    setTimeout(function() {
-        loader.style.display = 'none';
-        document.getElementById('content-of-this-web-site').style.display = 'block';
-        document.body.style.overflow = 'visible';
-    }, 350);
+    loader.style.display = 'none';
+    document.getElementById('content-of-this-web-site').style.display = 'block';
+    document.body.style.overflow = 'visible';
 }
 
 if (localStorage.getItem('navigated')) {
     hideLoader();
     localStorage.removeItem('navigated');
-} else {
-
-    document.getElementById('first-header-button').addEventListener('click', function() {
-        loader.style.display = 'block';
-        ctloader.style.display = 'flex';
-        document.getElementById('content-of-this-web-site').style.display = 'none';
-        
-        setTimeout(function() {
-            localStorage.setItem('navigated', 'true');
-            window.location.href = 'zayavka/register.html';
-        }, 500);
-    });
 }
 
+document.getElementById('first-header-button').addEventListener('click', function() {
+    loader.style.display = 'block';
+    ctloader.style.display = 'flex';
+    document.getElementById('content-of-this-web-site').style.display = 'none';
+    
+    setTimeout(function() {
+        localStorage.setItem('navigated', 'true');
+        window.location.href = 'zayavka/register.html';
+    }, 500);
+});
+
 window.addEventListener('load', function() {
-    hideLoader();
+    if (!localStorage.getItem('navigated')) {
+        hideLoader(); 
+    }
 });
 
 numberbarber.addEventListener('click', ()=> {
     window.location.href = 'tel: +7(996)5610404'
 })
+document.getElementById('registerBtn').addEventListener('click', function() {
+    window.location.href = 'zayavka/register.html';
+});
